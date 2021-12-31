09fc8ffa2099209f940021b41c47267693a7b0f3
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-12-30 22:41:46 -0400

    users migration data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,31 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->nullable();
+            $table->string('picture')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken()->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->datetime('last_online');
+            $table->string('verification_code')->nullable();
+            $table->string('new_email')->nullable();
+            $table->bigInteger('status')->nullable();
+            $table->bigInteger('first')->nullable();
+            $table->datetime('last_accept_date')->nullable();
+            $table->datetime('created')->nullable();
+            $table->datetime('modified')->nullable();
+            $table->string('company_contact')->nullable();
+            $table->double('credits', 8, 2)->nullable();
+            $table->integer('first_trip')->nullable();
+            $table->integer('incomplete_profile')->nullable();
+            $table->integer('phone_verify')->nullable();
+            $table->string('token_auto_login')->nullable();
+            $table->string('user_vertical')->nullable();
+            $table->integer('language_id')->nullable();
+            $table->integer('no_registered')->nullable();
+            $table->datetime('deleted_at')->nullable();
         });
     }
 
